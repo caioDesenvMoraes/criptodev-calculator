a5512047f96cf8b8419abcb892b97abffe82d8dd
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-22 14:44:07 -0300

    development footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,7 +4,18 @@ import "./Footer.css"
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <>
+            <footer className="footer">
+                <div className="container-footer">
+                    <p>Copyright © 2022 CM DEV - Todos os direitos reservados</p>
+                    <div className="social-itens">
+                        <a href="https://www.instagram.com/caiomutley/" target="_blank" rel="noreferrer">Instagram</a>
+                        <a href="https://www.linkedin.com/in/caio-moraes-8026a6158/" target="_blank" rel="noreferrer">Linkedin</a>
+                        <a href="https://github.com/caioDesenvMoraes" target="_blank" rel="noreferrer">Github</a>
+                    </div>
+                </div>
+            </footer>
+        </>
     )
 }
 
