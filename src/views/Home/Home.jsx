8c037a61e632d835a7cf17bf7310c6794193091a
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-22 13:07:10 -0300

    update home

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -20,13 +20,14 @@ const Home = () => {
                     <div className="description">
                         <h3>Descrição</h3>
                         <p>
-                            Projeto desenvolvido pelo curso cripto dev, aonde desenvolvi uma página web 
+                            Projeto desenvolvido no curso cripto dev, aonde desenvolvi uma página web 
                             utilizando a biblioteca react com o template em JavaScript, contendo duas 
-                            páginas, uma sendo a home, onde eu trago uma breve descrição sobre o projeto, 
-                            e a outra página contém a calculadora, onde podemos fazer as 4 principais 
-                            operações matemáticas, e desenvolvi também dois componentes, sendo um o 
-                            cabeçalho, aonde tem o meu logo e os links para acessarmos as duas páginas, 
-                            e o rodapé que contém a copy e os links para minhas redes sociais.
+                            páginas, uma sendo a home, onde eu trago minhas informações e uma breve 
+                            descrição sobre o projeto, e a outra página contém a calculadora, onde 
+                            podemos realizar as 4 principais operações matemáticas, e desenvolvi também 
+                            dois componentes, sendo um o cabeçalho, aonde tem o meu logo e os links para 
+                            acessarmos as duas páginas, e o rodapé que contém a copy e os links para 
+                            minhas redes sociais.
                         </p>
                     </div>
                 </div>
