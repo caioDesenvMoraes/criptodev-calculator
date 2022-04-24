4ac2ce3df7bffea5182eb960d68bb6b1b1983a33
Caio Moraes <caio.moraes.fbp@gmail.com>
2022-04-23 22:30:01 -0300

    develpment error

--- src/views/Err/Err.jsx
+++ src/views/Err/Err.jsx
@@ -1,10 +1,23 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 import "./Err.css"
 
 const Err = () => {
     return (
-        <div>Err</div>
+        <>
+            <div className="container-err">
+                <div className="content-err">
+                    <h1>Ops!</h1>
+                    <h2>404 Página não encontrada</h2>
+                    <p>O documento solicitado não existe ou foi removido</p>
+                    <div className="links">
+                            <Link to="/" className="item">Voltar para Home</Link>
+                            <Link to="/calculator" className="item">Ir para Calculadora</Link>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
 
